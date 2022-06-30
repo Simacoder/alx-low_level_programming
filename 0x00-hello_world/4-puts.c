4e3139b9c16763d5725596abf18c849820515073
Simacoder <simacoder@hotmail.com>
2022-06-30 04:48:03 -0700

    puts

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,7 +1,7 @@
 #include <stdio.h> /* the header file for c function*/
-/* Description: main- entry point for this function c*/
+/* main -> This is a print */
 int main(void)/*the main function */
 {
-puts("\"Programming is like building a multilingual puzzle\n"); /*puts line*/
+puts("\"Programming is like building a multilingual puzzle"); /*puts line*/
 return (0); /* the return function return zero for success*/
 }
