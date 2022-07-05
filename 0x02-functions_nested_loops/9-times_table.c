ef12bbae7dfbf1222d223108789cbfc04b3b6580
Simacoder <simacoder@hotmail.com>
2022-07-05 06:00:52 -0700

    time table

---
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -0,0 +1,16 @@
+#include "main.h"
+/**
+ * times_table -function that prints the 9 times table, starting with 0
+ * Rturn: Always 0 if success
+ */
+void times_table(void)
+{
+	int n, m;
+
+	for (n = 0; n <= 9; n++)
+	{
+		_putchar(m, n, m * n);
+		_putchar('\n');
+	}
+}
+
