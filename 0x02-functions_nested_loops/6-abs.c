c6e1fa8aad6ffd571f1a6991b50f0c396c94bde2
Simacoder <simacoder@hotmail.com>
2022-07-05 04:38:46 -0700

    abs

---
+++ 0x02-functions_nested_loops/6-abs.c
@@ -0,0 +1,12 @@
+#include "main.h"
+/**
+ * _abs - function that computes the absolute value of an integer
+ * @n: parameter member n
+ * Return: returns Always 0
+ */
+int _abs(int n)
+{
+if (n < 0)
+	n = -n;
+return (n);
+}
