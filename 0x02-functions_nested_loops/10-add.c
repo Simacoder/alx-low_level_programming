0cf580c915ddd14bb660c27539caf814a551694c
Simacoder <simacoder@hotmail.com>
2022-07-05 07:09:26 -0700

    adding

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,11 @@
+#include "main.h"
+/**
+ * add - function that adds two integers and returns the result.
+ * @x:parameter member
+ * @y:parameter member
+ * Return: x+ y always 0success
+ */
+int add(int x, int y)
+{
+	return (x + y);
+}
