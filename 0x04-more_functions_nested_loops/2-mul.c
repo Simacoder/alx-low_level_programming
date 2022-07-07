bb39375c03803e90c33456dc8ae3deac984978c4
Simacoder <simacoder@hotmail.com>
2022-07-07 15:59:07 -0700

    multiply by 2

---
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -0,0 +1,14 @@
+#include "main.h"
+
+/**
+ * mul -functin multiply by 2 numbers
+ * @a: parameter member
+ * @b: parameter member
+ * Return: success and integer
+ */
+
+int mul(int a, int b)
+{
+	return (a * b);
+}
+
