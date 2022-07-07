55d69180b794a384b568729768af8c16731843e5
Simacoder <simacoder@hotmail.com>
2022-07-07 06:47:48 -0700

    task 0

--- 0x03-debugging/0-main.c
+++ 0x03-debugging/0-main.c
@@ -1,13 +1,17 @@
 #include "main.h"
+
 /**
  * main - test the integer if it is positive or negative.
  * Return:Always 0
  */
+
 int main(void)
 {
 	int i;
 
 	i = 0;
 	positive_or_negative(i);
+
 	return (0);
+
 }
