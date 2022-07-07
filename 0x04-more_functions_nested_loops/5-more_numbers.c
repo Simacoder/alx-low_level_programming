2264f54b8aa24af8327e0b265fa3f37bf31bc56a
Simacoder <simacoder@hotmail.com>
2022-07-07 16:42:26 -0700

    more number

---
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -0,0 +1,21 @@
+#include "main.h"
+
+/**
+ * more_numbers - funtion that print 10 numbers
+ * Return: Always
+ */
+
+void more_numbers(void)
+{
+	int x, y;
+
+	for (x = 0; x < 10; x++)
+	{
+		for (y = 0; y <= 14; y++)
+		{
+			_putchar(y + '0');
+		}
+		_putchar('\n');
+	}
+}
+
