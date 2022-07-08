4224dc7bc001089b26c66563d020173019e41014
Simacoder <simacoder@hotmail.com>
2022-07-08 13:20:41 -0700

    triangle

---
+++ 0x04-more_functions_nested_loops/10-print_triangle.c
@@ -0,0 +1,33 @@
+#include "main.h"
+
+/**
+ * print_triangle - check for a digit numbers
+ * @size: parameter member
+ * Return: void
+ */
+
+void print_triangle(int size)
+{
+	int i = 1;
+	int j;
+
+	while (i <= size &&  size > 0)
+	{
+		j = 0;
+	while (j < size - 1)
+	{
+		_putchar(' ');
+		j++;
+	}
+	j = 0;
+	while (j < i)
+	{
+		_putchar('#');
+		j++;
+	}
+	_putchar('\n');
+	i++;
+	}
+if (i == 1)
+	_putchar('\n');
+}
