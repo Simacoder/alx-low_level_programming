9d716c58072027eb3bb84588e685b40dd7cf0e24
Simacoder <simacoder@hotmail.com>
2022-07-11 14:22:00 -0700

    reverse

---
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+ * print_rev -function that prints a string, in reverse, followed by a new line
+ * @s: parameter member
+ */
+
+void print_rev(char *s)
+{
+	int n, i;
+
+	n = strlen(s);
+	for (i = n - 1; i >= 0; i--)
+	{
+		_putchar(s[i]);
+	}
+	_putchar('\n');
+}
