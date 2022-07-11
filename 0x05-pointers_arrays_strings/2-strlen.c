d068c150b749bdee75ac4543852c995a3e437f67
Simacoder <simacoder@hotmail.com>
2022-07-11 13:54:35 -0700

    count strings

---
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/**
+ * _strlen - function that returns the length of a string
+ * @s: paramter member
+ * Return: return 0 sucess
+ */
+
+int _strlen(char *s)
+{
+	int count;
+
+	for (count = 0; s[count] != '\0'; s++
+	return (count);
+}
+
