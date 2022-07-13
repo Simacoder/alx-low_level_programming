983360a041c38811fbc73d6e221ee94dcec68248
Simacoder <simacoder@hotmail.com>
2022-07-13 12:27:42 -0700

    reverse array

--- 0x06-pointers_arrays_strings/4-rev_array.c
+++ 0x06-pointers_arrays_strings/4-rev_array.c
@@ -9,16 +9,12 @@
 
 void reverse_array(int *a, int n)
 {
-	int *p, i, aux, k;
+	int p, k;
 
-	p = a;
-	for (i = 0; i < n; i++)
-		p++;
-	for (k = 0; k < n / 2; k++)
+	for (k = n - 1; k > n / 2; k--)
 	{
-		aux = a[k];
-		a[k] = *p;
-		*p = aux;
-		p--;
+		p = a[n - 1 - k];
+		a[n - 1 - k] = a[k];
+		a[k] = p;
 	}
 }
