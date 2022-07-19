c16a1b87f2cd40dcdadd0ecaaefeeb5b2f8cfd5e
Simacoder <simacoder@hotmail.com>
2022-07-18 18:31:36 -0700

    lenght of string

---
+++ 0x07-pointers_arrays_strings/3-strspn.c
@@ -0,0 +1,26 @@
+#include "main.h"
+
+/**
+ * _strspn - print the consecutive character
+ * @s: parameter member
+ * @admit: parameter member
+ * Return: string
+ */
+
+unsigned int _strspn(char *s, char admit)
+{
+	unsigned int i, j;
+
+	for (j = 0; *(s + j); j++)
+	{
+		for (i = 0; *(admit + i); i++)
+		{
+			if (*(s + j) == *(admit + i))
+				break;
+		}
+		if (*(admit + i) == '\0')
+			break;
+	}
+	return (j);
+}
+
