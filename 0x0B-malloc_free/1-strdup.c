549f7d390900b0e154f1156d4411372a9f99046d
Simacoder <siimacoder@hotmail.com>
2022-07-25 21:08:38 -0700

    memory retun

---
+++ 0x0B-malloc_free/1-strdup.c
@@ -0,0 +1,28 @@
+#include "main.h"
+
+/**
+ * _strdup - function returns a pointer to a newly allocate mem
+ * @str: parameter member
+ * Return: string
+ */
+
+char *_strdup(char *str)
+{
+	int i = 1, j = 0;
+	char *s;
+
+	if (str == NULL)
+		return (NULL);
+	while (str[i])
+		i++;
+	s = (char *s)malloc(i * sizeof(char) + 1);
+	if (s == NULL)
+		return (NULL);
+	while (j < i)
+	{
+		s[j] = str[j];
+		j++;
+	}
+	s[j] = '\0';
+	return (s);
+}
