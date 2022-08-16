f182271ce0bdfe01e8c8fc6dae20d7e5b97e5d4a
Simacoder <simacoder@hotmail.com>
2022-08-15 18:14:43 -0700

    task o

---
+++ 0x13-more_singly_linked_lists/_putchar.c
@@ -0,0 +1,13 @@
+#include <unist.h>
+
+
+/**
+ * _putchar - functions that write out std
+ * @c: parameter member
+ * Return: suceess 1 or error -1
+ */
+
+int _putchar(char c)
+{
+	return (write(1, &c, 1));
+}
