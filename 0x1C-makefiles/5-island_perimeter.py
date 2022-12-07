b99a7be6e81746373b877b780b65833ec2937d48
Simanga <simacoder@hotmail.cm>
2022-12-07 12:59:19 -0800

    task 5

--- 0x1C-makefiles/5-island_perimeter.py
+++ 0x1C-makefiles/5-island_perimeter.py
@@ -4,6 +4,7 @@
 
 def island_perimeter(grid):
     """Function to calculate an island described in a grid perimeter
+
     grid is a list of list of integers:
     0 represents a water zone
     1 represents a land zone
@@ -20,6 +21,6 @@ def island_perimeter(grid):
                 size += 1
                 if (j > 0 and grid[i][j - 1] == 1):
                     edges += 1
-                    if (i > 0 and grid[i - 1][j] == 1):
-                        edges += 1
+                if (i > 0 and grid[i - 1][j] == 1):
+                    edges += 1
     return size * 4 - edges * 2
